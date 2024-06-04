f20798eb23c9f8b681657b509b2ff8c29785cdf4
Urveesh Dadhich <urveeshdadhich25@gmail.com>
2024-06-04 12:26:26 +0530

    created a test program to print hello world

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
-
-// Do not edit below this line
-module.exports = repeatString;
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  };
+  
+  module.exports = repeatString;
+  
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverse = ""; 
+    let pieces = [];
+    pieces = word.split("");
+    for(let i=pieces.length-1; i>=0; i--){
+    reverse += pieces[i];
+    }
+    return reverse;
 };
 
 // Do not edit below this line
